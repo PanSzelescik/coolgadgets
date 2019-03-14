b1e6e041b27f7318af32ef9821ee29b04df6954a
PanSzelescik <pan.szelescik20010131@gmail.com>
2019-03-14 18:41:58 +0100

    Switch from one branch

--- 1.12/src/main/java/panszelescik/coolgadgets/proxy/CommonProxy.java
+++
@@ -1,7 +0,0 @@
-package panszelescik.coolgadgets.proxy;
-
-public class CommonProxy {
-
-    public void init() {
-    }
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-This mod adds a few gadgets
-
-https://minecraft.curseforge.com/projects/coolgadgets
